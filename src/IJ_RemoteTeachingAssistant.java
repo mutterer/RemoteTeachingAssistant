b75e1f52dfebfbc3a1c6325537108f2f5596b931
mutterer <jerome.mutterer@gmail.com>
2021-03-22 16:29:47 +0100

    v1.01
    Thanks to @StillSciencing , this new version has command history and uses an image with text overlays.

--- src/IJ_RemoteTeachingAssistant.java
+++ src/IJ_RemoteTeachingAssistant.java
@@ -1,23 +1,16 @@
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.MessageDigest;
-
 import ij.CommandListener;
 import ij.Executer;
 import ij.IJ;
 import ij.ImagePlus;
-import ij.WindowManager;
 import ij.gui.GenericDialog;
+import ij.gui.ImageWindow;
+import ij.gui.Overlay;
+import ij.gui.TextRoi;
+import ij.plugin.Colors;
 import ij.plugin.PlugIn;
-import ij.text.TextWindow;
 
 /**
  * Adds a text window that displays the command being run
@@ -25,63 +18,74 @@ import ij.text.TextWindow;
  * @author jmutterer
  */
 
+
 public class IJ_RemoteTeachingAssistant implements PlugIn, CommandListener {
-	String t;
-	int fsize;
-	int delay;
-	TextWindow tw;
 
-	public void run(String arg) {
-		t = "Assistant";
-		
-		GenericDialog gd = new GenericDialog("IJ Remote Teaching Assistant");
-		gd.addNumericField("Font Size", 40);
-		gd.addNumericField("Display time (ms)", 1500);
-		gd.showDialog();
-		
-		if (gd.wasCanceled())
-			return;
-		
-		fsize = (int) gd.getNextNumber();
-		delay = (int) gd.getNextNumber();
+  ImagePlus imp;
+  Font f,f2;   
+  String cmds;
+  String fname;
+  String[] colors;
 
-		new TextWindow(t, "", 450, 60 + fsize);
-		tw = (TextWindow) WindowManager.getWindow(t);
-		tw.getTextPanel().setFont(new Font("Monospaced", Font.PLAIN, fsize), true);
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		tw.setLocation((int) (screenSize.getWidth() - tw.getWidth()), 0);
+  public void run(String arg) {
+    colors = new String[]{"black","green","orange"};
+    fname = TextRoi.getDefaultFontName();
+  
+    GenericDialog gd = new GenericDialog("IJ Remote Teaching Assistant");
+    gd.addStringField("Background Color", colors[0]);
+    gd.addStringField("Last command color", colors[1]);
+    gd.addStringField("History color", colors[2]);
+    gd.showDialog();
+    if (gd.wasCanceled())
+      return; 
+    colors[0] = gd.getNextString();    
+    colors[1] = gd.getNextString();    
+    colors[2] = gd.getNextString();
+  
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    ImageWindow.centerNextImage();
+    imp = IJ.createImage("Assistant", "8-bit", 600, 100, 1);
+    imp.getProcessor().setColor(Colors.decode(colors[0]));
+    imp.getProcessor().fill();
+    f = new Font(fname,Font.BOLD,40);
+    f2 = new Font(fname,Font.BOLD,30);
+    imp.show();
+    alert("Assistant ready.");
+    cmds="";
+    imp.getWindow().setLocation((int)(screenSize.getWidth() - imp.getWindow().getWidth()),0);
+    imp.getWindow().setBackground(Colors.decode(colors[0]));
+    Executer.addCommandListener(this);
+  }
+  public String commandExecuting(String command) {
+    if ((command=="Clear")&&IJ.getImage()==imp) {
+      cmds="";
+      alert("");
+      cmds="";
+    return null;}
+    else {
+      alert(command);
+      return command;
+    }
+  }
 
-		Executer.addCommandListener(this);
-
-	}
-
-	@Override
-	public String commandExecuting(String command) {
-		alert(command);
-		return command;
-	}
-
-	void alert(String s) {
-		if (WindowManager.getWindow(t) == null) {
-			Executer.removeCommandListener(this);
-		} else {
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						if (tw != null) {
-							tw.getTextPanel().setColumnHeadings(s);
-							tw.append("");
-							Thread.sleep(delay);
-							tw.getTextPanel().setColumnHeadings(" ");
-							tw.append("");
-						}
-					} catch (Exception e) {
-					}
-				}
-			}).start();
-		}
-		return;
-	}
-
+  void alert(String s) {
+    cmds = s+";"+cmds;
+    if (imp == null) {
+      Executer.removeCommandListener(this);
+    } else {
+      imp.setOverlay(null);
+      Overlay o = new Overlay();
+      TextRoi roi = new TextRoi(0, 0, s, f);
+      roi.setStrokeColor(Colors.decode(colors[1]));
+      roi.setJustification(TextRoi.LEFT);
+      String history = cmds.substring(cmds.indexOf(";")+1,Math.min(cmds.length(),60)).replaceAll(";"," | ");
+      TextRoi roi2 = new TextRoi(0, 50, (history.startsWith("null"))?"":history, f2);
+      roi2.setStrokeColor(Colors.decode(colors[2]));
+      roi2.setJustification(TextRoi.LEFT);      
+      o.add(roi);
+      o.add(roi2);
+      imp.setOverlay(o);
+    } 
+  }
 }
+
